b2d26e40e13f5b49eb3f91bf17c5378aec0f21f2
nightmare-mio <wanglongwei2009@qq.com>
2023-10-31 23:48:59 +0800

    新增表头格式

--- lib/headExcel.py
+++ lib/headExcel.py
@@ -1,7 +1,7 @@
 '''
 Author: nightmare-mio wanglongwei2009@qq.com
 Date: 2023-10-31 23:12:44
-LastEditTime: 2023-10-31 23:23:01
+LastEditTime: 2023-10-31 23:44:11
 Description: 
 '''
 from typing import Any
@@ -14,3 +14,4 @@ class headExcel(object):
         self.head_testscope=['序号', '功能模块', '功能名称', '角色', '责任人', '更新日期', '备注']
         self.head_outline= ['需求编号', '功能模块', '功能名称', '功能点', '用例类型', '检查点', '用例设计', '预期结果', '类别',
                 '责任人', '状态', '更新日期', '用例编号']
+        self.head_testcase_1=["storyid", '所属模块','子模块','用例标题', '前置条件', '步骤', '预期', '用例类型', '实际结果',"备注"]
